809b29b1db412536f794f0662977012dfc4df481
Femi Mehalayese <megafemclick@gmail.com>
2024-10-02 15:20:53 +0100

    spread connectoer

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,7 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const a = [...array1, ...array2];
-  for (const s of string) {
-    a.push(s);
-  }
-  return a;
+  return [...array1, ...array2, ...string];
 }
