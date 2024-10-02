f8e67d68825141cf84e18ee385e2f17245ce382d
Femi Mehalayese <megafemclick@gmail.com>
2024-10-02 15:17:11 +0100

    what is the problen naw

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,9 +1,7 @@
 export default function concatArrays(array1, array2, string) {
   const a = [...array1, ...array2];
   for (const s of string) {
-    if (s !== undefined) {
-      a.push(s);
-    }
+    a.push(s);
   }
   return a;
 }
