64f2037315fad6bf7c89ab6dd4ddb017e0f30a19
Femi Mehalayese <megafemclick@gmail.com>
2024-10-05 16:42:21 +0100

    class and price with converter

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+export default Pricing;
