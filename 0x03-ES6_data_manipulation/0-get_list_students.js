e554e88c75d45919c124ef39c83e81b67a59b51e
Femi Mehalayese <megafemclick@gmail.com>
2024-10-07 13:13:53 +0100

    get student

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function get_list_students() {
+  return [
+    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    {id: 2, firstName: 'James', location: 'Columbia'},
+    {id: 5, firstName: 'Serena', location: 'San Francisco'}
+  ];
+}
