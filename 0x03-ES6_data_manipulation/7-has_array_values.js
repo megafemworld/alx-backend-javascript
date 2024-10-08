90ecc08c5c9135beab313e972a1d5f2040b7e1be
Femi Mehalayese <megafemclick@gmail.com>
2024-10-08 22:56:03 +0100

    more set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  const setarray = new Set(array);
-  const diff = set.intersection(setarray);
-  if (diff.size == setarray.size) {
-    return true;
-  }
-  return false;
+  return array.every((value) => set.has(value));
 }
