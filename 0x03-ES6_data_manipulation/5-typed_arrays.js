cdd02379f419fa96c4a9e845c56fb95f00e88e88
Femi Mehalayese <megafemclick@gmail.com>
2024-10-08 17:29:46 +0100

    fixed

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,18 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (length instanceof number === false) {
-    throw new Error('Length must be a number');
-  }
-  if (position instanceof number === false) {
-    throw new Error('Position must be a number');
-  }
-  if (value instanceof number === false) {
-    throw new Error('Value must be a number');
-  }
   const buffer = new ArrayBuffer(length);
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
   const dataview = new DataView(buffer);
   dataview.setInt8(position, value);
-  return dataview
+  return dataview;
 }
