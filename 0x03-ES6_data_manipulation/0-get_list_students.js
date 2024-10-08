4fcfcf8a05219fdc7f4a403b6da53799d010766f
Femi Mehalayese <megafemclick@gmail.com>
2024-10-08 10:10:58 +0100

    tester 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,9 @@
-/**
- * Retrieves a list of students.
- * @author Femi <https://github.com/megafemworld>
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
 export default function getListStudents() {
-  return [
+  const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return students;
 }
