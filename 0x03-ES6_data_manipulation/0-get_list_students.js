8d60152901d6db0f87a1033187616a58cf3486b0
Femi Mehalayese <megafemclick@gmail.com>
2024-10-08 10:19:41 +0100

    students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,8 @@
-// Fichier : 0-get_list_students.js
-
-// La fonction getListStudents retourne le tableau d'objets d'étudiants spécifié dans l'énoncé.
-function getListStudents() {
-  return [
+export default function getListStudents() {
+  const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
+  return students;
 }
-
-// Exporte la fonction getListStudents pour pouvoir l'utiliser dans d'autres fichiers.
-export default getListStudents;
