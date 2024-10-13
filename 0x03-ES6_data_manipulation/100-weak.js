82ac3922eeca5b21c0f998076691ef3f85dfacb0
Femi Mehalayese <megafemclick@gmail.com>
2024-10-13 20:06:55 +0100

    weakmap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+const MAX_QUERY = 5;
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_QUERY) {
+    throw new Error("Endpoint load is high");
+  }
+}
