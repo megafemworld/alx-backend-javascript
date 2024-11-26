3edda511a86cd6c74bfc7bb6ecae0384251f5660
Femi Mehalayese <megafemclick@gmail.com>
2024-11-26 14:56:56 +0100

    stdin 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,6 @@ process.stdin.on('data', (data) => {
     if (data) {
         process.stdout.write(`Your name is: ${data}`);
     }
-    process.exit();
 });
 process.stdin.on('end', () => {
     process.stdout.write('This important software is now closing\n');
