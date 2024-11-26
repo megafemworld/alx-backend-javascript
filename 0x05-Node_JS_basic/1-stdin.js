0d72b6ebf550f5841b1e31f619c1fae97adc5023
Femi Mehalayese <megafemclick@gmail.com>
2024-11-26 14:23:24 +0100

    stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+    if (data) {
+        process.stdout.write(`Your name is: ${data}`);
+    }
+    process.exit();
+});
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
