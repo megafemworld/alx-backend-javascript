820f9296a6881b7b95408c816d10ae8aec99ae6b
Femi Mehalayese <megafemclick@gmail.com>
2024-12-09 15:08:55 +0100

    readfile sync

--- 0x05-Node_JS_basic/3-main_1.js
+++ 0x05-Node_JS_basic/3-main_1.js
@@ -1,19 +1,4 @@
-const countStudents = require('./3-read_file_async').default;
-
-countStudents("nope.csv")
-    .then(() => {
-        console.log("Done!");
-    })
-        .catch((error) => {
-        console.log(error);
-    });
-
-bob@dylan:~$ node 3-main_0.js
-Error: Cannot load the database
-...
-bob@dylan:~$
-bob@dylan:~$ cat 3-main_1.js
-const countStudents = require('./3-read_file_async').default;
+const countStudents = require('./3-read_file_async');
 
 countStudents("database.csv")
     .then(() => {
--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,13 +4,13 @@ function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        reject(Error('Cannot load the database'));
         return;
       }
-      const datas = data.toString('utf-8').trim().split('\n');
+      const datas = data.trim().split('\n');
       const studentRecords = {};
       const fileHead = datas[0].split(',');
-      const studentNames = fileHead.slice(0, fileHead.length - 1);
+      const StudentNames = fileHead.slice(0, fileHead.length - 1);
 
       for (const data of datas.slice(1)) {
         const studentData = data.split(',');
@@ -21,20 +21,20 @@ function countStudents(path) {
           studentRecords[field] = [];
         }
 
-        const dataEntry = studentNames
+        const dataEntry = StudentNames
           .map((name, index) => [name, studentMainData[index]]);
         studentRecords[field].push(dataEntry);
       }
 
+      console.log(`Number of students: ${datas.length - 1}`);
+
       for (const field in studentRecords) {
         if (Object.prototype.hasOwnProperty.call(studentRecords, field)) {
           const list = studentRecords[field];
-          const studentNames = list.map((item) => item[0][1]).join(', ');
-          console.log(`Number of students in ${field}: ${list.length}.`);
-          console.log(`List: ${studentNames}`);
+          console.log(`Number of students in ${field}: ${list.length}. List: ${list.map((item) => item[0][1]).join(', ')}`);
         }
       }
-      resolve({ studentRecords, totalStudents: datas.length - 1 });
+      resolve(true);
     });
   });
 }
