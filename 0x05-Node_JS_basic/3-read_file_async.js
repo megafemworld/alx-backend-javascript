b716b5fe8e9169b46c3702cee691ea7ecde891f5
Femi Mehalayese <megafemclick@gmail.com>
2024-12-09 12:25:26 +0100

    async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const f = require('fs');
+
+function countStudents(path) {
+  f.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const datas = data.toString('utf-8').trim().split('\n');
+    const studentRecords = {};
+    const fileHead = datas[0].split(',');
+    const StudentNames = fileHead.slice(0, fileHead.length - 1);
+
+    for (const data of datas.slice(1)) {
+      const studentData = data.split(',');
+      const studentMainData = studentData.slice(0, studentData.length - 1);
+      const field = studentData[studentData.length - 1];
+
+      if (!(field in studentRecords)) {
+        studentRecords[field] = [];
+      }
+
+      const dataEntry = StudentNames
+        .map((name, index) => [name, studentMainData[index]]);
+      studentRecords[field].push(dataEntry);
+    }
+
+    console.log(`Number of students: ${datas.length - 1}`);
+
+    for (const field in studentRecords) {
+      if (Object.prototype.hasOwnProperty.call(studentRecords, field)) {
+        const list = studentRecords[field];
+        console.log(`Number of students in ${field}: ${list.length}. List: ${list.map((item) => item[0][1]).join(', ')}`);
+      }
+    }
+  });
+}
+module.exports = countStudents;
