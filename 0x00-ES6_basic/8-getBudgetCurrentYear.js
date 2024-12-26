79ac7f71ce3044da38afca2822b3da1c4e2f11dc
Femi Mehalayese <megafemclick@gmail.com>
2024-12-26 18:58:38 +0100

    bufdget updte corrected

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,14 +5,12 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    income,
-    gdp,
-    capita,
+    [`income-${getCurrentYear()}`] : income,
+    [`gdp-${getCurrentYear()}`] : gdp,
+    [`capita-${getCurrentYear()}`] : capita,
   };
 
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
+
 
   return budget;
 }
