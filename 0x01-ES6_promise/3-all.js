afa7903856c385c8a32e9a55f2dab6e0fa7d2912
Femi Mehalayese <megafemclick@gmail.com>
2024-12-29 23:12:02 +0100

    all for all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto()
-    .then((data) => {
-      createUser()
-        .then((response) => {
-          console.log(data.body, response.firstName, response.lastName);
-        })
-        .catch(() => console.log('Signup system offline'));
-    });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((values) => console.log(values[0].body, values[1].firstName, values[1].lastName))
+    .catch(() => console.log('Signup system offline'));
 }
