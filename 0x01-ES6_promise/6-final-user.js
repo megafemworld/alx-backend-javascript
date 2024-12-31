cbe9341a22e4bf4ff0423671f543bc487a7516ac
Femi Mehalayese <megafemclick@gmail.com>
2024-12-31 21:06:42 +0100

    allsettled 1

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,13 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      for (const value of values) {
-        if (value.status === 'rejected') {
-          value.reason.message = `Error: ${value.reason.message}`;
-          value.reason.message = `Error: ${value.reason.message}`;
-        }
-      }
-      return values;
-    });
+    .then((values) => [
+      {
+        status: values.status,
+        value: values.value,
+      },
+    ]);
 }
